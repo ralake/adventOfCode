c7b8e5266e29edc61df0ec1fa204c314dcff0124
Richard Lake <richard.lake@qubit.com>
2015-12-18 18:56:21 +0000

    Update dayOneSpec.js

--- spec/dayOneSpec.js
+++ spec/dayOneSpec.js
@@ -2,18 +2,18 @@ import { FloorChecker } from '../src/dayOne.js'
 
 var fs = require('fs')
 var instructions = fs.readFileSync('./inputs/dayOne.txt', 'utf8', (err, data) => {
-	if (err) throw err
-	instructions = data
+  if (err) throw err
+  return data
 })
 
 let floorChecker = new FloorChecker(instructions)
 
-describe('FloorChecker', () => {
-	it('knows which floor you are on given some instructions', () => {
-		expect(floorChecker.findFloor()).toEqual(280)
-	})
+describe('FloorChecker', () => { 
+  it('knows which floor you are on given some instructions', () => {
+    expect(floorChecker.findFloor()).toEqual(280)
+  })
 
-	it('can tell when you are in the basement', () => {
-		expect(floorChecker.basementIndex()).toEqual(1797)
-	})
+  it('can tell when you are in the basement', () => {
+    expect(floorChecker.basementIndex()).toEqual(1797)
+  })
 })
