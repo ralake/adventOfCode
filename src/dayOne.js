b4d9627ee56570e151765746eff3e772c3a95bfa
Richard Lake <richard.lake@qubit.com>
2015-12-18 19:01:14 +0000

    Update dayOne.js

--- src/dayOne.js
+++ src/dayOne.js
@@ -4,29 +4,28 @@ export class FloorChecker {
   }
   findFloor(basement) {
     var level = 0
-		var index = 0
-		this.floors.forEach((floor) => {
-			if (floor === '(') {
-				level++
-			} else {
-				level--
-			}
-		})
-		return level
-	}
-	basementIndex() {
-		var level = 0
-		var index = 0
-		for (var i = 0; i < this.floors.length; i++) {
-			index++
-			if (this.floors[i] === '(') {
-				level++
-			} else {
-				level --
-				if (level === -1) break
-			}
-		}
-		return index
-	}
+    var index = 0
+    this.floors.forEach((floor) => {
+      if (floor === '(') {
+        level++
+      } else {
+        level--
+      }
+    })
+    return level
+  }
+  basementIndex() {
+    var level = 0
+    var index = 0
+    for (var i = 0; i < this.floors.length; i++) {
+      index++
+      if (this.floors[i] === '(') {
+        level++
+      } else {
+        level --
+        if (level === -1) break
+      }
+    }
+    return index
+  }
 }
-
