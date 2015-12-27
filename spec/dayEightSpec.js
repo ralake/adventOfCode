54c11ffbd979493c5fecb90c65dd97db820375f4
Richard Lake <ralake1985@gmail.com>
2015-12-27 16:39:35 +0000

    day Eight part one done

--- spec/dayEightSpec.js
+++ spec/dayEightSpec.js
@@ -7,8 +7,17 @@ let testInstructions = fs.readFileSync('./inputs/dayEightTest.txt', 'utf8', (err
   return data
 })
 
+let instructions = fs.readFileSync('./inputs/dayEight.txt', 'utf8', (err, data) => {
+  if (err) throw err
+  return data
+})
+
 describe('calculateStringLengths', () => {
-  it('can calculate the difference between the strings literal length and the length of its evalutation', () => {
+  it('can calculate the difference between the strings literal length and the length it takes in memory for the test case', () => {
     expect(calculateStringLengths(testInstructions)).toEqual(12)
+  })
+
+  it('can calculate the difference between the strings literal length and the length it takes in memory', () => {
+    expect(calculateStringLengths(instructions)).toEqual(1350)
   })
 })
