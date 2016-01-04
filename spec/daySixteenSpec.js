d8a71153392bef871b0756c2622acd2603edb7f9
Richard Lake <ralake1985@gmail.com>
2016-01-04 23:16:07 +0000

    finish day sixteen

--- spec/daySixteenSpec.js
+++ spec/daySixteenSpec.js
@@ -8,4 +8,8 @@ describe('auntieList', () => {
   it('can find the correct auntie from a list when given some evidence', () => {
     expect(auntieFinder(list, evidence)).toEqual(373)
   })
+
+  it('can find the correct auntie from a list when given evidence that includes ranges', () => {
+    expect(auntieFinder(list, evidence, true)).toEqual(260)
+  })
 })
--- src/daySixteen.js
+++ src/daySixteen.js
@@ -1,14 +1,24 @@
 let _ = require('underscore')
 
-export function auntieFinder (list, evidence) {
+export function auntieFinder (list, evidence, useRanges) {
   let aunties = getAunties(list)
   let evidenceData = getEvidenceData(evidence)
   let scores = _.map(aunties, (auntie) => {
-    let matchScore = 0
+    let auntieScore = 0
     _.each(auntie, (value, property) => {
-      if (_.has(evidenceData, property) && evidenceData[property] === value) matchScore++
+      if (_.has(evidenceData, property)) {
+        if (useRanges) {
+          let propertyIsTreesOrCats = /trees|cats/i.test(property)
+          let propertyIsPomeraniansOrGoldfish = /pomeranians|goldfish/.test(property)
+          if (propertyIsTreesOrCats && evidenceData[property] < value) auntieScore++
+          else if (propertyIsPomeraniansOrGoldfish && evidenceData[property] > value) auntieScore++
+          else if (!propertyIsTreesOrCats && !propertyIsPomeraniansOrGoldfish && evidenceData[property] === value) auntieScore++
+        } else {
+          if (evidenceData[property] === value) auntieScore++
+        }
+      }
     })
-    return matchScore
+    return auntieScore
   })
   let bestMatch = _.max(scores)
   return _.indexOf(scores, bestMatch) + 1
@@ -18,7 +28,7 @@ function getAunties (list) {
   return _.map(list.split('\n'), (auntie) => {
     auntie = auntie.replace(/Sue\s\d+:\s/g, '')
     let properties = auntie.match(/\b\w+\D\b/g)
-    let values = auntie.match(/\b\d+\b/g)
+    let values = _.map(auntie.match(/\b\d+\b/g), (number) => { return parseInt(number, 10) })
     return _.object(properties, values)
   })
 }
@@ -27,7 +37,7 @@ function getEvidenceData (evidence) {
   let data = {}
   evidence.split('\n').forEach((attribute) => {
     let propertyAndValue = attribute.match(/\b\w+\b/g)
-    data[_.first(propertyAndValue)] = _.last(propertyAndValue)
+    data[_.first(propertyAndValue)] = parseInt(_.last(propertyAndValue), 10)
   })
   return data
 }
